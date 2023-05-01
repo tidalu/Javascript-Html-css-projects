9a1c9cd986fd376c033c182d027bda75f348fc7a
Tidalu <127156318+tidalu@users.noreply.github.com>
2023-05-01 08:50:16 +0200

    reviewing

--- 9 video/app.js
+++ 9 video/app.js
@@ -8,21 +8,20 @@ const video = document.querySelector(".video-container");
 
 const preloader = document.querySelector(".preloader");
 
-btn.addEventListener("click", () => {
-    if (!btn.classList.contains("slide")) {
-        btn.classList.add("slide");
-        video.pause();
 
-    } else {
-        btn.classList.remove("slide");
+btn.addEventListener('click', ()=> {
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide');
+        video.pause();
+    }else{
+        btn.classList.remove('slide');
         video.play();
     }
 });
 
 
-//preloader
-
-window.addEventListener("load", () => {
-    preloader.classList.add("hide-preloader");
-})
+// preLoader 
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader');
+});
 
